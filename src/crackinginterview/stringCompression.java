ba9b5fd5317eb7d625c2121fbdb7c4d158ad5d5e
cathy0722 <chen312@usc.edu>
2017-08-02 22:20:19 -0700

    aa

--- src/crackinginterview/stringCompression.java
+++ src/crackinginterview/stringCompression.java
@@ -1,25 +1,30 @@
 package crackinginterview;
-
+//use StringBuilder for a dynamic String, save time
+//StringBuilder.append(char/string)
+//String.valueOf(int): string representation of int
 public class stringCompression {
 	static String string_compression(String s) {
-		char[] rt = new char[];
-		int i = 0;
-		int j = 1;
-		int index = 0;
-		while (j<s.length()) {
-			if (s.charAt(i) == s.charAt(j)) {
-				j++;
+//		String rt = "";
+		StringBuilder rt = new StringBuilder();
+		int count =0;
+		for (int i=0; i<s.length();i++){
+			if (i == s.length()-1 || s.charAt(i+1) != s.charAt(i)){
+				count++;                 
+//				rt += "" + s.charAt(i) + count;
+				rt.append(s.charAt(i));
+				rt.append(count);
+				count = 0;
 			}
 			else {
-				rt[index++] = s.charAt(i);
-				rt[index++] = Integer.toString(j-i+1).toCharArray();
+				count++;
 			}
 		}
-
+		return rt.length()<s.length()?rt.toString():s;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s = "aabbb";
+		System.out.println(string_compression(s));
 	}
 
 }
